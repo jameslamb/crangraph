2e6f877277b8ad6f2607f5916fde8abfb5156827
James Lamb <jaylamb20@gmail.com>
2017-04-24 20:36:15 -0500

    Update run_app.sh

--- ui/run_app.sh
+++ ui/run_app.sh
@@ -5,4 +5,4 @@ sudo /etc/rc.d/init.d/nginx start
 # Start up the app
 source activate crangraph
 cd $HOME/crangraph/ui
-gunicorn crangraph_ui:run -b localhost:8000 &
+gunicorn crangraph_ui:app -b localhost:8000 &
